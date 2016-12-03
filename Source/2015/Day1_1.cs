4227d62133c9cb730e04d76f53cd5c39dd0e1d76
Per Westerlund <per@fastmail.com>
2016-12-03 13:46:34 +0100

    Added accepted solution + data for 2015/day1

---
+++ Source/2015/Day1_1.cs
@@ -0,0 +1,79 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+using static System.Console;
+
+namespace Day2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new Program().Run(args);
+        }
+
+        public void Run(string[] args)
+        {
+            IReader reader;
+            if (args.Length > 0)
+            {
+                reader = new FileReader(args[0]);
+            }
+            else
+            {
+                reader = new ConsoleReader();
+            }
+
+            var input = reader.ReadLine();
+            int sum = 0;
+            foreach (var c in input)
+            {
+                if (c == '(') sum ++;
+                if (c == ')') sum --;
+            }
+
+            WriteLine(sum);
+        }
+    }
+
+    interface IReader
+    {
+        string ReadLine();
+        bool EndOfStream { get; }
+    }
+
+    public class ConsoleReader : IReader
+    {
+        public string ReadLine()
+        {
+            return Console.ReadLine();
+        }
+
+        public bool EndOfStream => false;
+    }
+
+    public class FileReader : IReader
+    {
+        StreamReader _reader;
+        
+        public FileReader(string filename)
+        {
+            _reader = new StreamReader(filename);
+        }
+
+        public string ReadLine()
+        {
+            return _reader.ReadLine();
+        }
+
+        public bool EndOfStream 
+        {
+            get
+            {
+                return _reader.EndOfStream;
+            }
+        }
+    }
+}
