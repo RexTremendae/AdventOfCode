1f6c30ee78325285f1a8ad24ebb793e722fd811f
Per Westerlund <per@fastmail.com>
2016-12-05 22:55:54 +0100

    [2015:10] Added accepted solution to part 1

---
+++ Source/2015/Day10_1.cs
@@ -0,0 +1,136 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+using static System.Console;
+
+namespace Day2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new Program().Run(args);
+        }
+
+        public void Run(string[] args)
+        {
+            IReader reader;
+            if (args.Length > 0)
+            {
+                reader = new FileReader(args[0]);
+            }
+            else
+            {
+                reader = new ConsoleReader();
+            }
+
+            var input = reader.ReadLine();
+
+            int lastLength = 0;
+            for (int i = 0; i < 40; i++)
+            {
+                string newString = string.Empty;
+                int idx = 0;
+                while (idx < input.Length)
+                {
+                    char current = input[idx];
+                    int counter = 0;
+                    
+                    while (idx < input.Length && input[idx] == current)
+                    {
+                        idx++;
+                        counter++;
+                    }
+
+                    newString += counter.ToString();
+                    newString += current;
+                }
+
+                //WriteLine(newString);
+                input = newString;
+                lastLength = newString.Length;
+            }
+
+            WriteLine(lastLength);
+        }
+    }
+
+    interface IReader
+    {
+        string ReadLine();
+        bool EndOfStream { get; }
+    }
+
+    public class ConsoleReader : IReader
+    {
+        public string ReadLine()
+        {
+            return Console.ReadLine();
+        }
+
+        public bool EndOfStream => false;
+    }
+
+    public class FileReader : IReader
+    {
+        StreamReader _reader;
+        
+        public FileReader(string filename)
+        {
+            _reader = new StreamReader(filename);
+        }
+
+        public string ReadLine()
+        {
+            return _reader.ReadLine();
+        }
+
+        public bool EndOfStream 
+        {
+            get
+            {
+                return _reader.EndOfStream;
+            }
+        }
+    }
+
+    public class Point
+    {
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public int X { get; }
+
+        public int Y { get; }
+
+        public static Point Parse(string input)
+        {
+            var split = input.Split(',');
+            int x = int.Parse(split[0]);
+            int y = int.Parse(split[1]);
+
+            return new Point(x, y);
+        }
+
+        public override int GetHashCode()
+        {
+            return Tuple.Create(X, Y).GetHashCode();
+        }
+
+        public override bool Equals(object other)
+        {
+            Point otherPoint = other as Point;
+            if (otherPoint == null)
+            {
+                return false;
+            }
+
+            return X == otherPoint.X && Y == otherPoint.Y;
+        }
+    }
+}
