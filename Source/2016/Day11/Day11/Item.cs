0f3b1c9ea59e379543b089702e70448a22174c9f
Per Westerlund <per@fastmail.com>
2016-12-11 23:06:07 +0100

    [2016:11] Added visualization

---
+++ Source/2016/Day11/Day11/Item.cs
@@ -0,0 +1,89 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Day11
+{
+    public class Item : INotifyPropertyChanged
+    {
+        public Item(string id)
+        {
+            Id = id;
+        }
+
+        FontWeight _fontWeight;
+        private SolidColorBrush _outerBorderBrush;
+        private SolidColorBrush _innerBorderBrush;
+        private SolidColorBrush _backgroundBrush;
+        private bool _isSelected;
+        private string _displayText;
+
+        public string DisplayText
+        {
+            get { return _displayText; }
+            set
+            {
+                _displayText = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public bool IsSelected
+        {
+            get { return _isSelected; }
+            set
+            {
+                _isSelected = value;
+                if (_isSelected)
+                {
+                    OuterBorderBrush = new SolidColorBrush(Colors.Black);
+                    InnerBorderBrush = new SolidColorBrush(Colors.Black);
+                }
+                else
+                {
+                    OuterBorderBrush = new SolidColorBrush(Colors.Transparent);
+                    InnerBorderBrush = new SolidColorBrush(Colors.Gray);
+                }
+            }
+        }
+
+        public SolidColorBrush BackgroundBrush
+        {
+            get { return _backgroundBrush; }
+            set { _backgroundBrush = value; RaisePropertyChanged(); }
+        }
+
+        public SolidColorBrush OuterBorderBrush
+        {
+            get { return _outerBorderBrush; }
+            set { _outerBorderBrush = value; RaisePropertyChanged(); }
+        }
+
+        public SolidColorBrush InnerBorderBrush
+        {
+            get { return _innerBorderBrush; }
+            set { _innerBorderBrush = value; RaisePropertyChanged(); }
+        }
+
+        public FontWeight FontWeight
+        {
+            get { return _fontWeight; }
+            set { _fontWeight = value; RaisePropertyChanged(); }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void RaisePropertyChanged([CallerMemberName]string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public string Id { get; }
+
+        internal void ToggleSelected()
+        {
+            IsSelected = _isSelected ? false : true;
+        }
+    }
+}
