8b7671933c19c4719285c4c8a8c6a27c6b9810fb
Per Westerlund <per@fastmail.com>
2017-12-05 08:55:50 +0100

    [2017:05] Added accepted solution

---
+++ Source/2017/Day5_1.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"strconv"
+    "bufio"
+    "fmt"
+    "log"
+    "os"
+)
+
+func main() {
+	file, err := os.Open("input.txt")
+    if err != nil {
+        log.Fatal(err)
+    }
+    defer file.Close()
+    scanner := bufio.NewScanner(file)
+
+    instructions := []int { }
+
+    for scanner.Scan() {
+        currInstr, err := strconv.Atoi(scanner.Text())
+        if (err != nil) {
+            log.Fatal(err)
+        }
+        instructions = append(instructions, currInstr);
+    }
+
+    lastIdx := len(instructions) - 1
+    stepCounter := 0
+    currIdx := 0
+
+    for {
+        prevIdx := currIdx
+        currIdx += instructions[currIdx]
+        instructions[prevIdx] ++
+        stepCounter ++
+
+        if (currIdx > lastIdx) {
+            break;
+        }
+    }
+
+    fmt.Println(stepCounter)
+}
