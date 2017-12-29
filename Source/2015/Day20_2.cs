9e95900722508112eb29f12c841986483f642393
Per Westerlund <per@fastmail.com>
2017-12-29 22:43:11 +0100

    Added accepted solution to [2015] Day 20

---
+++ Source/2015/Day20_2.cs
@@ -0,0 +1,117 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using static System.Console;
+using static System.ConsoleColor;
+using static Day20.ColorWriter;
+
+namespace Day20
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            new Program().Run(args);
+        }
+
+        public void Run(string[] args)
+        {
+            long max = 2_000_000;
+            var data = new long [max+1];
+            long goal = 29_000_000;
+
+            long i = 1;
+            for (;i <= max;)
+            {
+                int count = 0;
+                for (long j = i; j <= max && count < 50; j += i, count++)
+                {
+                    data[j] += i*11;
+                }
+
+                if (i % 10_000 == 0)
+                {
+                    WriteLine($"Progress: {i:### ### ### ###} - {data[i]:### ### ### ###}");
+                }
+
+                if (data[i] >= goal)
+                {
+                    WriteLine($"GOAL reached: {i:### ### ### ###} - {data[i]:### ### ### ###}");
+                    break;
+                }
+
+                i++;
+            }
+        }
+
+        long[] CloneAndIncrease(long[] data, long max)
+        {
+            var ret = new long[max];
+            for(long i = 0; i < data.Length; i++)
+            {
+                ret[i] = data[i];
+            }
+
+            return ret;
+        }
+    }
+
+    interface IReader
+    {
+        string ReadLine();
+        bool EndOfStream { get; }
+    }
+
+    public class ConsoleReader : IReader
+    {
+        public string ReadLine()
+        {
+            return Console.ReadLine();
+        }
+
+        public bool EndOfStream => false;
+    }
+
+    public class FileReader : IReader
+    {
+        StreamReader _reader;
+        
+        public FileReader(string filename)
+        {
+            _reader = new StreamReader(filename);
+        }
+
+        public string ReadLine()
+        {
+            return _reader.ReadLine();
+        }
+
+        public bool EndOfStream 
+        {
+            get
+            {
+                return _reader.EndOfStream;
+            }
+        }
+    }
+
+    public static class ColorWriter
+    {
+        public static void Write(string text, ConsoleColor color)
+        {
+            var oldColor = ForegroundColor;
+            ForegroundColor = color;
+            Console.Write(text);
+            ForegroundColor = oldColor;
+        }
+
+        public static void WriteLine(string text, ConsoleColor color)
+        {
+            Write(text, color);
+            Console.WriteLine();
+        }
+    }
+}
