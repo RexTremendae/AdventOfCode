23ce8d2ea2b77d866cbdb4bcd29cf2163e32ffbc
Per Westerlund <per@fastmail.com>
2018-12-05 19:45:30 +0100

    [2018:04] Added accepted solution, Part 1

---
+++ Source/2018/Day4_1.rs
@@ -0,0 +1,136 @@
+use std::fs;
+use std::collections::HashMap;
+
+fn main() {
+    let contents = fs::read_to_string("Day4.txt")
+        .expect("Something went wrong reading the file");
+
+    let mut all_lines = contents.split("\n").collect::<Vec<&str>>();
+    all_lines.sort();
+    let mut _guard_id = -1;
+    let mut _total_sleep:HashMap<i32,i32> = HashMap::new();
+    let mut _sleep_minutes:HashMap<(i32, i32), i32> = HashMap::new();
+    let mut _sleep_start = 0;
+
+    for line in all_lines
+    {
+        if line == "" { continue; }
+
+        //    0         1      2    3    4      5
+        //[1518-11-01 00:00] Guard #10 begins shift
+        //[1518-11-01 00:05] falls asleep
+        //[1518-11-01 00:25] wakes up
+
+        let parts = line.split(" ").collect::<Vec<&str>>();
+        let _time = parts[1][..5].split(":").collect::<Vec<&str>>();
+        let _hour = _time[0].parse::<i32>().unwrap();
+        let _min = _time[1].parse::<i32>().unwrap();
+        let _date = parts[0][1..].split("-").collect::<Vec<&str>>();
+        let mut _year = _date[0].parse::<i32>().unwrap();
+        let mut _month = _date[1].parse::<i32>().unwrap();
+        let mut _day = _date[2].parse::<i32>().unwrap();
+
+        let (_year, _month, _day) = get_guard_date(_year, _month, _day, _hour);
+
+        if parts[2] == "Guard"
+        {
+            _guard_id = parts[3][1..].parse::<i32>().unwrap();
+            //println!("[{}-{}-{}] Guard {}", _year, _month, _day, _guard_id);
+            continue;
+        }
+        else if parts[2] == "falls"
+        {
+            _sleep_start = _min;
+            //println!("[{}-{}-{}] Guard {} falls asleep at {}", _year, _month, _day, _guard_id, _min);
+            continue;
+        }
+        else if parts[2] == "wakes"
+        {
+            //println!("[{}-{}-{}] Guard {} wakes up at {}", _year, _month, _day, _guard_id, _min);
+            *_total_sleep.entry(_guard_id).or_insert(0) += _min-_sleep_start;
+            for mut i in _sleep_start.._min
+            {
+                *_sleep_minutes.entry((_guard_id, i)).or_insert(0) += 1;
+            }
+
+            continue;
+        }
+    }
+
+    let mut max_id = 0;
+    let mut max_time = 0;
+    for (id, time) in _total_sleep
+    {
+        if max_time < time
+        {
+            max_time = time;
+            max_id = id;
+        }
+    }
+
+    let mut max_count = 0;
+    let mut max_minute = 0;
+    for minute in 0..60
+    {
+        match _sleep_minutes.get(&(max_id, minute))
+        {
+            Some(count) =>
+            {
+                if *count > max_count
+                {
+                    max_count = *count;
+                    max_minute = minute;
+                }
+            },
+            None => { }
+        }
+    }
+
+    println!("{}", max_id * max_minute);
+}
+
+fn get_guard_date(mut year: i32, mut month: i32, mut day: i32, hour: i32) -> (i32, i32, i32)
+{
+    if hour == 0
+    {
+        return (year, month, day);
+    }
+
+    day = day + 1;
+
+    // 31 day month
+    if [1, 3, 5, 7, 8, 10, 12].contains(&month)
+    {
+        if day > 31
+        {
+            day = 1;
+            month = month + 1;
+        }
+    }
+    // 30 day month
+    else if [4, 6, 9, 11].contains(&month)
+    {
+        if day > 30
+        {
+            day = 1;
+            month = month + 1;
+        }
+    }
+    // February
+    else
+    {
+        if day > 28
+        {
+            day = 1;
+            month = month + 1;
+        }
+    }
+
+    if month > 12
+    {
+        year = year + 1;
+        month = 1;
+    }
+
+    (year, month, day)
+}
