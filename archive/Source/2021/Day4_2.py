be7ec51e73ed04f0c80745df942ec4ca24c9439f
Per Westerlund <per@fastmail.com>
2021-12-04 12:36:01 +0100

    [2021:04] Added accepted python solution for part 1+2

---
+++ archive/Source/2021/Day4_2.py
@@ -0,0 +1,53 @@
+file = open("Day4.txt", "r")
+
+numbers = []
+first = True
+boards = []
+currentBoard = ({}, [], []) # rowidx, data, marks
+rowIdx = 0
+
+for line in file:
+  line = line.rstrip()
+  if first:
+    numbers = line.split(',')
+    first = False
+  elif line == '':
+    if len(currentBoard[0]) > 0:
+      boards.append(currentBoard)
+      currentBoard = ({}, [], [])
+      currentBoardMarks = []
+      rowIdx = 0
+  else:
+    row = line.split()
+    colIdx = 0
+    for r in row:
+      currentBoard[0][r] = (rowIdx, colIdx)
+      colIdx += 1
+    currentBoard[1].append(row)
+    currentBoard[2].append([False for r in row])
+    rowIdx += 1
+boards.append(currentBoard)
+
+wins = []
+for n in numbers:
+  for b in range(len(boards)):
+    curr = boards[b]
+    if n in curr[0]:
+      idx = curr[0][n]
+      curr[2][idx[0]][idx[1]] = True
+      if len(set(curr[2][idx[0]])) == 1 or len(set([d[idx[1]] for d in curr[2]])) == 1:
+        if not b in wins: wins.append(b)
+  if len(wins) == len(boards): break
+
+win = wins[-1]
+winb = boards[win]
+print (f"Board {win} wins last")
+sum = 0
+for row in range(len(winb[2])):
+  for col in range(len(winb[2][row])):
+    mrk = winb[2][row][col]
+    data = winb[1][row][col]
+    if not mrk:
+      sum += int(winb[1][row][col])
+
+print (sum*int(n))
