6c3a04ca4a17af01c26d2d51dc0f3c2e4586e15d
Per Westerlund <per@fastmail.com>
2021-12-12 09:58:13 +0100

    [2021:12] Added accepted python solution for part 1+2

---
+++ 2021/Day12_1.py
@@ -0,0 +1,129 @@
+def main():
+  map = get_map(puzzle_input)
+
+  path_count = 0
+
+  q = []
+  q.append(('start', ['start'], []))
+
+  while len(q) > 0:
+    (pos, visited, path) = q[0]
+    q.pop(0)
+
+    if pos == 'end':
+      path.append(pos)
+      #print (f"FOUND: {path}")
+      path_count += 1
+      continue
+
+    if pos not in map: continue
+
+    for dest in filter(lambda d: d not in visited, map[pos]):
+      visit_clone = visited.copy()
+      path_clone = path.copy()
+
+      if dest.islower():
+        visit_clone.append(dest)
+
+      path_clone.append(pos)
+
+      q.append((dest, visit_clone, path_clone))
+
+  print (path_count)
+
+
+def get_map(input):
+  map = {}
+  for path in filter(lambda p: p != "", input.split()):
+    parts = path.split('-')
+    _from = parts[0]
+    _to = parts[1]
+
+    if not _from in map:
+      map[_from] = []
+    map[_from].append(_to)
+
+    if not _to in map:
+      map[_to] = []
+    map[_to].append(_from)
+
+  return map
+
+small_example = \
+"""
+start-A
+start-b
+A-c
+A-b
+b-d
+A-end
+b-end
+"""
+
+slightly_larger_example = \
+"""
+dc-end
+HN-start
+start-kj
+dc-start
+dc-HN
+LN-dc
+HN-end
+kj-sa
+kj-HN
+kj-dc
+"""
+
+even_larger_example = \
+"""
+fs-end
+he-DX
+fs-he
+start-DX
+pj-DX
+end-zg
+zg-sl
+zg-pj
+pj-he
+RW-he
+fs-DX
+pj-RW
+zg-RW
+start-pj
+he-WI
+zg-he
+pj-fs
+start-RW
+"""
+
+puzzle_input = \
+"""
+xq-XZ
+zo-yr
+CT-zo
+yr-xq
+yr-LD
+xq-ra
+np-zo
+end-LD
+np-LD
+xq-kq
+start-ra
+np-kq
+LO-end
+start-xq
+zo-ra
+LO-np
+XZ-start
+zo-kq
+LO-yr
+kq-XZ
+zo-LD
+kq-ra
+XZ-yr
+LD-ws
+np-end
+kq-yr
+"""
+
+main()
